7d24e382f6063f1a3571239338d0fcfdaa16c498
fzabid05 <abidzora88@gmail.com>
2020-01-07 17:37:13 +0100

    article

---
+++ src/Entity/Recette.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RecetteRepository")
+ */
+class Recette
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ingredient;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $etapes;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getIngredient(): ?string
+    {
+        return $this->ingredient;
+    }
+
+    public function setIngredient(string $ingredient): self
+    {
+        $this->ingredient = $ingredient;
+
+        return $this;
+    }
+
+    public function getEtapes(): ?string
+    {
+        return $this->etapes;
+    }
+
+    public function setEtapes(string $etapes): self
+    {
+        $this->etapes = $etapes;
+
+        return $this;
+    }
+}
